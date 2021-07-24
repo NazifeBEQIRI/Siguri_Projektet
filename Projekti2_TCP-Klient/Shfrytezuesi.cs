dde1e4270fe40f5a13e47e35a6208324f659e082
LiridonaBaliu <75494050+LiridonaBaliu@users.noreply.github.com>
2021-07-24 14:50:06 +0100

    Add files via upload

---
+++ Projekti2_TCP-Klient/Shfrytezuesi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Klienti
+{
+    public class Shfrytezuesi
+    {
+        public string userId { get; set; }
+        public string PasswordHash { get; set; }
+        public string emriMbiemri { get; set; }
+        public string llojiFatures { get; set; }
+        public int viti { get; set; }
+        public decimal vleraEuro { get; set; }
+        public string muaji { get; set; }
+        public string email { get; set; }
+    }
+}
